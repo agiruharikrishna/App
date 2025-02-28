faa07e406746abe33ac4d9817d5f71b583235de3
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-28 21:36:31 +0530

    Update Student.java

--- src/main/java/com/studentapp/model/Student.java
+++ src/main/java/com/studentapp/model/Student.java
@@ -1,16 +1,22 @@
 package com.studentapp.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String password;
+
+    private boolean enabled = true;
 
     // Getters and Setters
 }
