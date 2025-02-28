bf59188c7043c730adf26c500b1db4d227e4f3a4
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-28 21:37:13 +0530

    Update StudentRepository.java

--- src/main/java/com/studentapp/repository/StudentRepository.java
+++ src/main/java/com/studentapp/repository/StudentRepository.java
@@ -1,10 +1,11 @@
 package com.studentapp.repository;
 
-import com.studentapp.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.studentapp.model.Student;
 
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByName(String name);
 }
+
