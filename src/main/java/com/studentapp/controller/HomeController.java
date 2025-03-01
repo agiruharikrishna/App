150a7513a600cdcd09b4a14a3ecdb2187f4f3331
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-03-01 06:51:52 +0530

    Update HomeController.java

--- src/main/java/com/studentapp/controller/HomeController.java
+++ src/main/java/com/studentapp/controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    // Remove or modify the /home mapping
+    // Use a different URL if needed
+    @GetMapping("/dashboard")  // Updated path
     public String homePage() {
         return "home";  // This will render home.html in the templates folder
     }
