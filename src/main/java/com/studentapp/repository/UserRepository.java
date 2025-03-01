908bfcb8355a7adfe1a0541d8d41afff47b5ddf0
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-03-01 07:54:12 +0530

    Update UserRepository.java

--- src/main/java/com/studentapp/repository/UserRepository.java
+++ src/main/java/com/studentapp/repository/UserRepository.java
@@ -4,6 +4,5 @@ import com.studentapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods if needed
+    // Custom queries can be added here if needed
 }
-
