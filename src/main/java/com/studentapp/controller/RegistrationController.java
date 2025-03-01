5ca07e4c0e5ee38adad0127dd66dbf53bd33835b
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-03-01 21:30:47 +0530

    Update RegistrationController.java

--- src/main/java/com/studentapp/controller/RegistrationController.java
+++ src/main/java/com/studentapp/controller/RegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import com.studentapp.model.User;
 
 @Controller
 public class RegistrationController {
