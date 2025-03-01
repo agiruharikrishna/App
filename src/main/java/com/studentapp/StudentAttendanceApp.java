c0c9fd0e3167a7caaddfe7b859adfa063cfccd4f
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-03-01 14:21:47 +0530

    Update StudentAttendanceApp.java

--- src/main/java/com/studentapp/StudentAttendanceApp.java
+++ src/main/java/com/studentapp/StudentAttendanceApp.java
@@ -3,9 +3,11 @@ package com.studentapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.studentapp.model")  // Adjust the package name where your JPA entities are located
+@EntityScan(basePackages = "com.studentapp.model")  // Ensure the package where entities are located
+@EnableJpaRepositories(basePackages = "com.studentapp.repository")  // Ensure the repository package is correctly scanned
 public class StudentAttendanceApp {
     public static void main(String[] args) {
         // Starts the Spring Boot application
