3edda65b8241835a39e2a585713ea068ce464497
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-03-01 22:14:06 +0530

    Update User.java

--- src/main/java/com/studentapp/model/User.java
+++ src/main/java/com/studentapp/model/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strateg =  GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
